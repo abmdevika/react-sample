226b9128d8623da47ecfe190018bfb98648a2c55
devika <abmdevika@gmail.com>
2020-01-12 15:19:32 +0530

    Updates

---
+++ precache-manifest.49e121fa1240f9f4093fb6f62b8bcd56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38f4b12e1acd8a5dc2285f05d8d517f0",
+    "url": "/react-sample/index.html"
+  },
+  {
+    "revision": "4cc247078cf2c3c8b197",
+    "url": "/react-sample/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c46591e142b2a01137f7",
+    "url": "/react-sample/static/js/2.ef4efc17.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-sample/static/js/2.ef4efc17.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4cc247078cf2c3c8b197",
+    "url": "/react-sample/static/js/main.d9f89a2c.chunk.js"
+  },
+  {
+    "revision": "a44372b2c7b939b39eca",
+    "url": "/react-sample/static/js/runtime-main.7d8b5f46.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-sample/static/media/logo.5d5d9eef.svg"
+  }
+]);
